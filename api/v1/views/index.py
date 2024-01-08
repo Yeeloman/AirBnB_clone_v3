04ffb9cab60aa629107b1f195669aa7d8bb97700
Yandelf00 <zakaria.afyf00@gmail.com>
2024-01-08 18:56:41 +0100

    add

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,22 @@
 """Create a Route `/status` on the object app_views."""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
 def jsonstatus():
     """the return ok status"""
     return jsonify(status='OK')
+
+
+@app_views.route('/api/v1/stats', methods=['GET'])
+def getstats():
+    """endpoint that retrives the number of each obcts by type"""
+    dicreturn = {"amenities": storage.count('amenities'),
+                 "cities": storage.count('cities'),
+                 "places": storage.count('places'),
+                 "reviews": storage.count('reviews'),
+                 "states": storage.count('states'),
+                 "users": storage.count('users')}
+    return jsonify(dicreturn)
